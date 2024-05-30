10f628224c363ce5b0f56d38f341d4b4e17d38fe
Renke Brixel <93048457+ReynkeDeVos@users.noreply.github.com>
2024-05-30 13:49:16 +0200

    Backend: Added prettier for consistent code stylings

--- Backend/routes/pokemonRoutes.js
+++ Backend/routes/pokemonRoutes.js
@@ -2,11 +2,9 @@ const express = require('express');
 const router = express.Router();
 const pokemonController = require('../controllers/pokemonController');
 
-
 router.get('/', pokemonController.getAllPokemon);
 
 router.get('/:id', pokemonController.getPokemonById);
-
 
 router.get('/:id/:info', pokemonController.getPokemonInfoById);
 

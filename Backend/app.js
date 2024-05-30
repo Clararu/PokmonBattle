d6f0edab4f4f0a62e5883ca27637f3c690b17951
clararu <clararueck@gmail.com>
2024-05-30 10:32:31 +0200

    Kommentare

--- Backend/app.js
+++ Backend/app.js
@@ -6,13 +6,11 @@ const pokemonRoutes = require('./routes/pokemonRoutes');
 const app = express();
 app.use(cors());
 app.use(express.json());
-// EJS를 뷰 엔진으로 설정
+
 app.set('view engine', 'ejs');
 
-// 미들웨어로 라우트를 사용
 app.use('/pokemon', pokemonRoutes);
 
-// CORS 미들웨어를 등록
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
